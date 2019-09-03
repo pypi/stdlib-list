40b37a924a84f0f989ca80dabda01b413f59363c
Sylvain MARIE <sylvain.marie@se.com>
2019-09-03 11:43:32 +0200

    New method `in_stdlib` to efficiently check if a module name is part of std lib. It relies on functools' `@lru_cache` to cache the stdlib list and query results for similar calls. Therefore it is much more efficient than `module_name in stdlib_list()` especially if you wish to perform multiple checks.

--- stdlib_list/__init__.py
+++ stdlib_list/__init__.py
@@ -3,4 +3,4 @@ __version__ = get_versions()['version']
 del get_versions
 
 # Import all the things that used to be in here for backwards-compatibility reasons
-from .base import stdlib_list, get_canonical_version, short_versions, long_versions, base_dir, list_dir
+from .base import stdlib_list, in_stdlib, get_canonical_version, short_versions, long_versions, base_dir, list_dir
