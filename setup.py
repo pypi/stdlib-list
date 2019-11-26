23cc36cea3409aa26989b93b4f893053b2008dc9
Filipe Fernandes <ocefpaf@gmail.com>
2019-11-26 11:37:01 -0300

    use only markdown readme

--- setup.py
+++ setup.py
@@ -1,27 +1,21 @@
-import setuptools
+from setuptools import setup, find_packages
 
 import versioneer
 
 
-try:
-    with open('README.rst') as f:
-        long_description = f.read()
-except IOError:
-    long_description = ""
-
-
-setuptools.setup(
-    name='stdlib-list',
-    license='MIT',
-    author='Jack Maney',
-    author_email='jackmaney@gmail.com',
-    url='https://github.com/jackmaney/python-stdlib-list',
+setup(
+    name="stdlib-list",
+    license="MIT",
+    author="Jack Maney",
+    author_email="jackmaney@gmail.com",
+    url="https://github.com/jackmaney/python-stdlib-list",
     version=versioneer.get_version(),
-    install_requires=['functools32;python_version<"3.2"'],
+    install_requires=["functools32;python_version<'3.2'"],
     extras_require={"develop": ["sphinx"]},
-    description='A list of Python Standard Libraries (2.6-7, 3.2-6).',
-    long_description=long_description,
+    description="A list of Python Standard Libraries (2.6-7, 3.2-6).",
+    long_description=f'{read("README.md")}',
+    long_description_content_type="text/markdown",
     include_package_data=True,
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     cmdclass=versioneer.get_cmdclass(),
 )
