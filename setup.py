980dc4918ef22b6bc3a9289c664c189d463903d0
Filipe Fernandes <ocefpaf@gmail.com>
2019-11-26 11:38:34 -0300

    update description

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     version=versioneer.get_version(),
     install_requires=["functools32;python_version<'3.2'"],
     extras_require={"develop": ["sphinx"]},
-    description="A list of Python Standard Libraries (2.6-7, 3.2-6).",
+    description="A list of Python Standard Libraries (2.6-7, 3.2-8).",
     long_description=f'{read("README.md")}',
     long_description_content_type="text/markdown",
     include_package_data=True,
