8ae1b6cebb2fefc3fa70b6403b47f34f300d3bd1
William Woodruff <william@yossarian.net>
2023-06-14 20:26:18 -0400

    [WIP] newer python support

--- support/walk-modules.py
+++ support/walk-modules.py
@@ -7,7 +7,7 @@ import sys
 SEEN_MODS = set()
 
 
-def walk_pkgutil(mod_name, mod):
+def walk_pkgutil(mod_name, mod, io):
     for pkg in pkgutil.walk_packages(mod.__path__, mod_name + "."):
         if pkg.name in SEEN_MODS:
             continue
@@ -15,10 +15,10 @@ def walk_pkgutil(mod_name, mod):
             # We don't recurse here because `walk_packages` takes care of
             # it for us.
             SEEN_MODS.add(pkg.name)
-            print(pkg.name)
+            print(pkg.name, file=io)
 
 
-def walk_naive(mod_name, mod):
+def walk_naive(mod_name, mod, io):
     for attr in dir(mod):
         attr_obj = getattr(mod, attr, None)
         # Shouldn't happen, but who knows.
@@ -35,38 +35,45 @@ def walk_naive(mod_name, mod):
         try:
             submod_name = mod_name + "." + attr
             __import__(submod_name)
-            walk(submod_name)
+            walk(submod_name, io)
         except ImportError:
             # ...but sometimes we do want to include re-exports, since
             # they might be things like "accelerator" modules that don't
             # appear anywhere else.
             # For example, `_bz2` might appear as a re-export.
-            walk(attr)
+            walk(attr, io)
 
 
-def walk(mod_name):
+def walk(mod_name, io):
     if mod_name in SEEN_MODS:
         return
     else:
         SEEN_MODS.add(mod_name)
-        print(mod_name)
+        print(mod_name, file=io)
 
     # Try and import it.
     try:
         mod = __import__(mod_name)
 
         if hasattr(mod, "__path__"):
-            walk_pkgutil(mod_name, mod)
+            walk_pkgutil(mod_name, mod, io)
         else:
-            walk_naive(mod_name, mod)
+            walk_naive(mod_name, mod, io)
 
     except ImportError:
         pass
 
 
 if __name__ == "__main__":
-    for mod_name in sys.stdin:
-        walk(mod_name.rstrip("\n"))
+    output = sys.argv[1]
 
-    for mod_name in sys.builtin_module_names:
-        walk(mod_name)
+    with open(output, mode="w") as io:
+        for mod_name in sys.builtin_module_names:
+            walk(mod_name, io)
+
+        if hasattr(sys, "stdlib_module_names"):
+            for mod_name in sys.stdlib_module_names:
+                walk(mod_name, io)
+        else:
+            for mod_name in sys.stdin:
+                walk(mod_name.rstrip("\n"), io)
